f43a9ea8c9a9811e6f805a5032ff1ceb37872452
Grayson <136826478+Gr4ys0n@users.noreply.github.com>
2025-04-22 12:28:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button
+let mybutton = document.getElementById("version");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
